82de87c9d58c861f4a7ff23ef2b03d02e8bdcd15
koreatogetherfactory <koreatogether@zoho.com>
2025-08-03 20:58:28 +0900

    refactor: Remove unused Arduino mock files and related test utilities

--- test/test_utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-#include <string>
-#include "mocks/MockTemperatureSensor.h"
-
-// 유틸리티 함수 선언
-std::vector<MockTemperatureSensor::Entry> getMockData();
-std::string makeTableLine(int idx, const MockTemperatureSensor::Entry &entry);
